81b3a95a97593fd852520563e5e3080a92e34b39
aethrox <69115453+aethrox@users.noreply.github.com>
2024-09-01 20:36:20 +0300

    Log system updated

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 import express, { json, urlencoded } from "express";
 import morgan from "morgan";
+import fs from "fs";
+import path from "path";
 import UserRoutes from "./routes/user.route.js";
 
 const app = express();
 
+const accessLogStream = fs.createWriteStream(path.join(path.resolve(), "access.log"), { flags: "a" });
+
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(morgan("dev", ":method :url :status :res[content-length] - :response-time ms"));
+app.use(morgan("combined", { stream: accessLogStream }));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
